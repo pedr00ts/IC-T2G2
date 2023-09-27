d067fb6959032ffd1d501cd24fac373902c0a82c
negrigabriel <negrigabriel@ua.pt>
2023-09-27 11:34:06 +0100

    args validation

--- sndfile-example-src/wav_hist.cpp
+++ sndfile-example-src/wav_hist.cpp
@@ -9,8 +9,11 @@ constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
 int main(int argc, char *argv[]) {
 
-	if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <channel>\n";
+	if(argc < 3 || argc > 4) {
+		cerr << "Usage: " << argv[0] << " <input file> <option>\n";
+		cerr << "-m for MID";
+		cerr << "-s for SIDE";
+		cerr << "-c <channel> for desired channel";
 		return 1;
 	}
 
@@ -30,21 +33,46 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	int channel { stoi(argv[argc-1]) };
-	if(channel >= sndFile.channels()) {
-		cerr << "Error: invalid channel requested\n";
+	string option { argv[2] };
+	int channel { };
+	if (option == "-c") {
+		if (argc != 4) {
+				cerr << "Error: missing desired channel\n";
+				return 1;
+			}
+
+		channel = stoi(argv[argc-1]);
+		if(channel >= sndFile.channels()) {
+			cerr << "Error: invalid channel requested\n";
+			return 1;
+		}
+	} else {
+		cerr << "Error: invalid option. Valid options are:\n";
+		cerr << "-m for MID";
+		cerr << "-s for SIDE";
+		cerr << "-c <channel> for desired channel";
 		return 1;
 	}
+
 
 	size_t nFrames;
 	vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
 	WAVHist hist { sndFile };
 	while((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
 		samples.resize(nFrames * sndFile.channels());
-		hist.update(samples);
+		
+		if (option == "-c") {
+			hist.update(samples);
+		} else if (option == "-m") {
+			//hist.update_mid(samples);
+		} else {
+			//hist.update_mid(samples);
+		}
 	}
 
-	hist.dump(channel);
+	if (option == "-c") {
+		hist.dump(channel);
+	}
 	return 0;
 }
 
