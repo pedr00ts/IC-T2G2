73406bc1168cc4382ea9282c427168200192af40
negrigabriel <negrigabriel@ua.pt>
2023-09-27 14:10:47 +0100

    hist working

--- sndfile-example-src/wav_hist.cpp
+++ sndfile-example-src/wav_hist.cpp
@@ -7,17 +7,23 @@ using namespace std;
 
 constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 
+void usage(char *argv[]) {
+	cerr << "Usage: " << argv[0] << " <input file> <option>\n";
+	cerr << "	List of available options:\n";
+	cerr << "	-c <channel>  for desired channel\n";
+	cerr << "	-m            for MID\n";
+	cerr << "	-s            for SIDE\n";
+}
+
 int main(int argc, char *argv[]) {
 
 	if(argc < 3 || argc > 4) {
-		cerr << "Usage: " << argv[0] << " <input file> <option>\n";
-		cerr << "-m for MID";
-		cerr << "-s for SIDE";
-		cerr << "-c <channel> for desired channel";
+		cerr << "Error: wrong number of args\n";
+		usage(argv);
 		return 1;
 	}
 
-	SndfileHandle sndFile { argv[argc-2] };
+	SndfileHandle sndFile { argv[1] };
 	if(sndFile.error()) {
 		cerr << "Error: invalid input file\n";
 		return 1;
@@ -32,47 +38,49 @@ int main(int argc, char *argv[]) {
 		cerr << "Error: file is not in PCM_16 format\n";
 		return 1;
 	}
-
+	
+	// validar opcao selecionada
 	string option { argv[2] };
+
+	WAVHist::Type type { };
 	int channel { };
 	if (option == "-c") {
 		if (argc != 4) {
-				cerr << "Error: missing desired channel\n";
-				return 1;
-			}
+			cerr << "Error: missing desired channel number\n";
+			usage(argv);
+			return 1;
+		}
 
 		channel = stoi(argv[argc-1]);
 		if(channel >= sndFile.channels()) {
 			cerr << "Error: invalid channel requested\n";
 			return 1;
 		}
+
+		type = WAVHist::Type::CHANNELS;
+	} else if (option == "-m") {
+		type = WAVHist::Type::MID;
+	} else if(option == "-s") {
+		type = WAVHist::Type::SIDE;
 	} else {
-		cerr << "Error: invalid option. Valid options are:\n";
-		cerr << "-m for MID";
-		cerr << "-s for SIDE";
-		cerr << "-c <channel> for desired channel";
+		cerr << "Error: invalid option.\n";
+		usage(argv);
 		return 1;
 	}
 
 
 	size_t nFrames;
 	vector<short> samples(FRAMES_BUFFER_SIZE * sndFile.channels());
-	WAVHist hist { sndFile };
+	WAVHist hist { sndFile, type };
 	while((nFrames = sndFile.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
 		samples.resize(nFrames * sndFile.channels());
-		
-		if (option == "-c") {
-			hist.update(samples);
-		} else if (option == "-m") {
-			//hist.update_mid(samples);
-		} else {
-			//hist.update_mid(samples);
-		}
+		hist.update(samples);
 	}
 
-	if (option == "-c") {
-		hist.dump(channel);
-	}
+	if (type == WAVHist::Type::CHANNELS)
+		hist.dump_counts(channel);
+	else
+		hist.dump();
 	return 0;
 }
 
--- sndfile-example-src/wav_hist.h
+++ sndfile-example-src/wav_hist.h
@@ -10,24 +10,36 @@
 #include <sndfile.hh>
 
 class WAVHist {
+	public:
+	enum Type {
+		CHANNELS, MID, SIDE
+	};
+
   private:
 	std::vector<std::map<short, size_t>> counts;
 	std::map<short, size_t> mid;				
 	std::map<short, size_t> side;
+	Type type;
 
   public:
 	WAVHist(const SndfileHandle& sfh) {
 		counts.resize(sfh.channels());
+		type = Type::CHANNELS;
 	}
 
-	void update(const std::vector<short>& samples) {
+	WAVHist(const SndfileHandle& sfh, const Type aType) {
+		counts.resize(sfh.channels());
+		type = aType;
+	}
+
+	// Atualiza o histograma counts.
+	void update_counts(const std::vector<short>& samples) {
 		size_t n { };
 		for(auto s : samples)
 			counts[n++ % counts.size()][s]++;
 	}
 
-
-//	Atualiza o histograma mid de acordo com os valores em counts.
+	// Atualiza o histograma mid.
 	void update_mid(const std::vector<short>& samples) {
 		short freq = 0;
 		short c = 0;
@@ -40,6 +52,7 @@ class WAVHist {
 		}
 	}
 
+	// Atualiza o histograma side.
 	void update_side(const std::vector<short>& samples) {
 		if (counts.size() != 2)
 			std::cout << "Error in update_side: function requires two channels" + '\n';
@@ -60,38 +73,54 @@ class WAVHist {
 		}
 	}
 
-	/*void update_mid(const std::vector<short>& samples) {
-		float sum;
-		for (auto s : samples){
-
-			sum = 0;
-			for (int i = 1; i <= counts.size(); i++) {
-				sum += s;
-			}
+	void update(const std::vector<short>& samples) {
+		switch (type) {
+			case Type::CHANNELS:
+				update_counts(samples);
+				break;
+			case Type::MID:
+				update_mid(samples);
+				break;
+			case Type::SIDE:
+				update_side(samples);
+				break;
+			default:
+				break;
 		}
-		counts[0][round(sum/counts.size())]++;
-
-			
-	} */
+	}
 
 
+	// Escreve no stdout os valores atuais de counts. 
+	void dump_counts(const int channel) const {
 
-	void dump(const size_t channel) const {
 		for(auto [value, counter] : counts[channel])
 			std::cout << value << '\t' << counter << '\n';
 	}
 
-// Escreve no stdout os valores atuais de mid. 
+	// Escreve no stdout os valores atuais de mid. 
 	void dump_mid() const {
 		for(auto [value, counter] : mid)
 			std::cout << value << '\t' << counter << '\n';
 	}
 
-// Escreve no stdout os valores atuais de side. 
+	// Escreve no stdout os valores atuais de side. 
 	void dump_side() const {
 		for(auto [value, counter] : side)
 			std::cout << value << '\t' << counter << '\n';
 	}	
+
+	void dump() const {
+		switch (type) {
+			case Type::MID:
+				dump_mid();
+				break;
+			case Type::SIDE:
+				dump_side();
+				break;
+			default:
+				break;
+		}
+	}
 };
 
 #endif
