44b002f1aae367f1c7e8381a05d17ea64c75dcfb
pedr00ts <pedrots@ua.pt>
2023-10-13 12:33:01 +0100

    Implementação meanSquaredError

--- sndfile-example-src/wav_cmp.h
+++ sndfile-example-src/wav_cmp.h
@@ -14,40 +14,34 @@ using namespace std;
 class WavCmp {
 
     private:
-        SndfileHandle fileA;
-        SndfileHandle fileB;
         vector <short> samplesA;
-        vector <short> samplesB;  
+        vector <short> samplesB;
+        size_t nChannels; 
 
     public:
 
 
-    WavCmp(const SndfileHandle& file1, const SndfileHandle& file2) {
-        if (file1.channels() != file2.channels()) {
+    WavCmp(SndfileHandle& fileA, SndfileHandle& fileB) {
+        if (fileA.channels() != fileA.channels()) {
             cerr << "Error - provided files have incompatible number of channels\n";
             return;
         }
-        fileA = file1;
-        fileB = file2;
+        nChannels = fileA.channels();
 
         // Read all frames from fileA to samplesA
-        size_t totalFrames;
         size_t nFrames {} ;       
         do {
             samplesA.reserve(fileA.channels() * FRAMES_BUFFER_SIZE);
-            totalFrames += nFrames;
         } while((nFrames = fileA.readf((short*)samplesA.back(), FRAMES_BUFFER_SIZE)));
 
         // Read all frames from fileB to samplesB
-        totalFrames = 0;
         nFrames = 0;
         do {
             samplesB.reserve(fileB.channels() * FRAMES_BUFFER_SIZE);
-            totalFrames += nFrames;
-        } while((nFrames = fileA.readf((short*)samplesA.back(), FRAMES_BUFFER_SIZE)));
+        } while((nFrames = fileA.readf((short*)samplesA.back(), FRAMES_BUFFER_SIZE))); // nframes > 0 --> loop
 
         if (samplesA.size() != samplesB.size()) {
-            cerr << "Error - provided files have incompatible sizes\n";
+            cerr << "Error - provided files have incompatible amount of samples\n";
             return;
         }
 
@@ -57,12 +51,38 @@ class WavCmp {
 
 
 
-
+    // return meanSquaredError between samplesA and samplesB for specified channel
     double meanSquaredError (short channel) {
-        double result { };
+        double res { };
+        size_t i = channel;
+        
+        while (i < samplesA.size()) {
+            res += abs(pow(samplesA[i], 2) - pow(samplesB[i], 2));
+            i += nChannels;
+        }
+        
+        return res/nChannels;
+    }
 
+    // return meanSquaredError between samplesA and samplesB for average of channels
+    double meanSquaredErrorAvg () {
+        double res { };
+        double sumA { };
+        double sumB { };
+         
+        for (size_t i = 0; i < samplesA.size(); i = (i+1) % nChannels) {
+            sumA += samplesA[i];
+            sumB += samplesB[i];
 
+            // i == nChannels-1 -> calculate MSE between A and B samples and reset sums  
+            if (i == (nChannels - 1)) {
+                res += abs(pow((sumA/nChannels), 2) - pow((sumA/nChannels), 2));
+                sumA = 0;
+                sumB = 0;
+            }
+        }
 
+        return res/(samplesA.size()/nChannels); // size / nChannels == nElements ()
     }
 
 
