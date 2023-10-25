e0cc7b1423900c669031441e0aa21c63f75d92f3
negrigabriel <negrigabriel@ua.pt>
2023-10-25 17:30:55 +0100

    effects

--- sndfile-example-src/wav_effects.h
+++ sndfile-example-src/wav_effects.h
@@ -6,25 +6,58 @@
 
 using namespace std;
 
-class Wav_Effects {
+class Wav_Effect {
+    public:
+        virtual vector<short> apply(vector<short>& in) {return in;}
+};
 
+class Wav_Delay: public Wav_Effect {
     private:
-        vector<short> samples;
-        samplesOut
+        float time;
+        int feedback;
 
     public:
-        Wav_Effects (vector<short> values) {
-            samples = values;
+        Wav_Delay (float time, int feedback) {
+            Wav_Delay::time = time;
+            Wav_Delay::feedback = feedback;
         }
 
+        vector<short> apply(vector<short>& in) {
+            vector<short> out = out;
+            return out;
+        }
+};
 
+class Wav_Echo: public Wav_Effect {
+    private:
+        float delay;
+        float decay;
 
+    public:
+        Wav_Echo (float delay, float decay) {
+            Wav_Echo::delay = delay;
+            Wav_Echo::decay = decay;
+        }
 
+        vector<short> apply(vector<short>& in) {
+            vector<short> out = in;
+            return out;
+        }
+};
 
+class Wav_Reverb: public Wav_Effect {
+    private:
+        float delay;
+        float decay;
 
+    public:
+        Wav_Reverb (float delay, float decay) {
+            Wav_Reverb::delay = delay;
+            Wav_Reverb::decay = decay;
+        }
 
-
-
-
-
+        vector<short> apply(vector<short>& in) {
+            vector<short> out = in;
+            return out;
+        }
 };
