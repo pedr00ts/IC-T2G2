1f31b1c76c64a26d4027eaf10a32dae53960db9b
pedr00ts <pedrots@ua.pt>
2023-10-25 15:41:58 +0100

    wav_effects

---
+++ sndfile-example-src/wav_effects.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <sndfile.hh>
+#include <cmath>
+#define  FRAMES_BUFFER_SIZE 65536
+
+using namespace std;
+
+class Wav_Effects {
+
+    private:
+        vector<short> samples;
+        samplesOut
+
+    public:
+        Wav_Effects (vector<short> values) {
+            samples = values;
+        }
+
+
+
+
+
+
+
+
+
+
+
+};
