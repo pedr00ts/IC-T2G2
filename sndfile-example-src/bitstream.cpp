5af7327a62e8a7200a7e7d8b6bb0d32a1fc8eff2
pedr00ts <pedrots@ua.pt>
2023-10-28 19:50:36 +0100

    Ex 5

--- sndfile-example-src/bitstream.cpp
+++ sndfile-example-src/bitstream.cpp
@@ -4,7 +4,51 @@ using namespace std;
 
 // USED FOR TESTING EX. 5
 
+void testWrite() {
+    BitStream bs {};
+    fstream testfile("test.txt");
+    vector<bool> bits(56);
+    testfile.seekp(testfile.beg);
+    for (char i = 'A'; i < 'G'; i++) {
+        /*for (size_t j = 0; j < 8; j++) {
+            switch(j) {
+                case 0:
+                    bits[j] = bits[j] & BitStream::bit7;
+                    break;
+                case 1:
+                    bits[j] = bits[j] & BitStream::bit6;
+                    break;
+                case 2:
+                    bits[j] = bits[j] & BitStream::bit5;
+                    break;
+                case 3:
+                    bits[j] = bits[j] & BitStream::bit4;
+                    break;
+                case 4:
+                    bits[j] = bits[j] & BitStream::bit3;
+                    break;
+                case 5:
+                    bits[j] = bits[j] & BitStream::bit2;
+                    break;
+                case 6:
+                    bits[j] = bits[j] & BitStream::bit1;
+                    break;
+                case 7:
+                    bits[j] = bits[j] & BitStream::bit0;
+            }
+            
+        }*/
+        //bs.writeByte(testfile, i);
+    }
+    
+    //bs.writeNBits(testfile, bits, 0);
+
+    testfile.close();
+}
+
 int main(int argc, char *argv[]) {
+
+    testWrite();
 
     BitStream f{};
     
@@ -14,12 +58,15 @@ int main(int argc, char *argv[]) {
     f.writeBit(file, 1, 0);
     f.writeBit(file, 1, 5);
     f.writeBit(file, 0, 1);
-    f.writeBit(file, 0, 7);    
+    f.writeBit(file, 0, 7);
+    f.writeBit(file, 1, 8);
+    f.writeBit(file, 1, 1);   
 
-    // test readBit
     file.seekg(0, file.end);
     size_t charcount = file.tellg();
-    file.seekg(0, file.beg);
+    file.seekg(0, file.beg); 
+
+    // test readBit
     cout << "charcount: " << charcount << '\n';
 
     for (int i = 0; i < charcount; i++) {
@@ -30,6 +77,11 @@ int main(int argc, char *argv[]) {
         }
         cout << '\n';
     }
+
+    file.seekg(file.beg);
+    vector readValues = f.readNBits(file, 64, 0);
+    for (auto value : readValues)
+        cout << value;
 
     file.close();
 
--- sndfile-example-src/bitstream.h
+++ sndfile-example-src/bitstream.h
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 class BitStream {
     private:
-//        ifstream fileIn;
-//        ofstream fileOut;
 
         // convert variable in bits to corresponding value in bytes.
         // NOT TESTED
@@ -16,6 +15,12 @@ class BitStream {
                 return bits;
             else 
                 return (0 << bits/8) & bits;
+        }
+
+    public:
+
+        BitStream() {
+
         }
 
         enum MASK {
@@ -29,21 +34,11 @@ class BitStream {
             bit7 = 0b00000001,
         };
 
-    public:
-
-        BitStream() {
-
-        }
-
-//        void setFileIn(ifstream& inputStream){
-//            fileIn = inputStream;
-//        }
-
         // reads bit at position pos from the stream
         // requires correct position in the stream
         bool readBit(fstream& fileIn, size_t pos) {
 
-            // read final byte from the stream
+            // read byte from the stream
             char byte;
             fileIn.get(byte);
             
@@ -73,14 +68,33 @@ class BitStream {
             return 1;
         }
 
-        // writes bit to byte at the end of file, in position pos 
-        void writeBit(fstream& fileIn, bool bit, size_t pos) {
-            char* byte = new char[1];
+        // reads N bits from the stream, starting at position pos
+        vector<bool> readNBits(fstream& fileIn, size_t N, size_t pos) {
+            vector<bool> values(N);
+            for (int i = 0; i < N; i++) {
+                values[i] = readBit(fileIn, pos);
+                if (pos < 7)
+                    fileIn.seekg(fileIn.tellg()-1);
+                pos = (pos+1) % 8;
+            }
+            return values;
+        }
+
+        // writes bit to byte at the end of file, in position pos
+        // pos > 7 -> write new byte with value 0x00
+        int writeBit(fstream& fileIn, bool bit, size_t pos) {
+            // create new byte
+            if (pos > 7) {
+                //fileIn.seekp(0, fileIn.end);
+                fileIn.put(bit << 7);
+                return 0;
+            } 
 
             // read final byte
+            char* byte = new char[1];           
             fileIn.seekg(-1, fileIn.end);
             fileIn.get(byte[0]);
-
+            
             cout << "Original byte: " << byte[0] << '\n';
             
             // modify byte
@@ -96,13 +110,108 @@ class BitStream {
             // write to file
             fileIn.seekp(fileIn.tellg()-1);
             fileIn.write(byte, 1);
+
+            return 0;
         }
 
-        // reads byte from provided stream
-        char readByte(ifstream& fileIn) {
-            char byte;
-            fileIn.get(byte);
-            return byte;
+        // 
+        int writeNBits(fstream& fileIn, const vector<bool>& values, size_t pos) {
+            size_t N = values.size();
+            size_t i = 0;
+
+            if(pos > 7) {
+                cerr << "Error in writeNBits: invalid pos value\n";
+                return 1;
+            }
+
+            // adjust byte position
+            while(pos != 8) {
+                if (i == N)
+                    return 0;
+                writeBit(fileIn, values[i++], pos++);
+            }
+
+            // write byte blocks
+            char* byte;
+            while((N-i) / 8 > 0) {
+                byte[0] = 0x00;
+                for (size_t j = 0; j < 7; j++) {
+                    byte[0] = (byte[0] << 1) | values[i+j];
+                }
+                fileIn.write(byte, 1);
+                i += 8;
+            }
+
+            pos = 0;
+            while (i < N) {
+                writeBit(fileIn, values[i++], pos++);
+            }
+
+            return 0;
+        }
+
+
+        // reads N bytes from provided stream
+        vector<char> readNBytes(fstream& fileIn, size_t N) {
+            vector<char> values(N);
+            for(size_t i = 0; i < N; i++) {
+                fileIn.get(values[i]);
+            }
+            return values;
+        }
+
+        // writes bytes to provided stream
+        void writeBytes(fstream& fileIn, vector<char> values) {
+            fileIn.write(&values[0], values.size());
+        }
+
+        // converts byte vector to bit vector (most significant to least)
+        vector<bool> decode(vector<char> bytes) {
+            vector<bool> bits(bytes.size()*8);
+            size_t i = 0;
+            size_t mask = 0x80;
+            for (char byte : bytes) {
+                do {
+                    bits[i++] = (mask >> (i % 8)) & byte;
+                } while(i % 8 != 0);
+            }
+            return bits;
+        }
+
+        // converts bit vector to byte vector
+        vector<char> encode(vector<bool> bits) {
+            size_t size = bits.size()/8;
+            if (bits.size()%8 != 0)
+                size++;
+            vector<char> bytes(size);
+            char byte = 0x00;
+            for (size_t i = 1; i <= size; i++) {
+                byte = byte | bits[i];
+                if(i % 8 == 0) {
+                    bytes[i/8] = byte;
+                    byte = 0x00;
+                }
+                byte << 1;
+            }
+            if (bits.size() % 8 != 0)
+                bytes[size-1] = bytes[size-1] << (7 - bits.size() % 8);
+            return bytes;   
+        }
+
+        string readString(fstream& fileIn) {
+            string res {};
+            char* next;
+            while (fileIn.peek() != '\0') {
+                fileIn.get(next[0]);
+                res.append(next); 
+            }
+            return res;
+        }
+
+        void writeString(fstream& fileIn, string s) {
+            char c = s[0];
+            while(c != '\0')
+                fileIn.write(&c, 1);
         }
 
 
