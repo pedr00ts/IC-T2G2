ae0b56e2765662cdfce4a9ffaf11ed0498ddabf2
Pedro Sugiyama <pedrots@ua.pt>
2023-11-27 16:24:06 +0000

    Interleaving mode

--- Projeto2/golomb.cpp
+++ Projeto2/golomb.cpp
@@ -13,26 +13,39 @@ class Golomb {
         
         vector<bool> prefix(int n) {
             vector<bool> vec {};
-            
-            for (uint32_t i = 0; i < (uint32_t)n/m; i++)    // insert n/m 0's in vector
-                vec.push_back(0);
-            
-            vec.push_back(1);                               // insert terminator 1
-            return vec;
 
-            // INCLUDE MODE 1: MULTIPLY N BY 2; ADD 1 IF N < 0 
+            if (mode){
+                n *= 2;
+                if (n < 0){ 
+                    n--;
+                }
+            }
+
+            for (uint32_t i = 0; i < n/m; i++)    // insert n/m 0's in vector
+                vec.push_back(0);
+            vec.push_back(1);                     // insert terminator 1
+    
+            return vec;
         }
 
         vector<bool> suffix(int n) {
             vector<bool> vec{};
-            u_int8_t bitcount = floor(log2(n));
-            u_int32_t rem = ((uint32_t)n) % m;              // remainder used in suffix
-            if (ceil(log2(n)) == bitcount) {                // m is a power of 2 --> no extended bitcount
-                for (uint32_t i = bitcount-1; i >= 0; i--) 
-                    vec.push_back(rem >> i);
-                
-            } else {                                        // m not power of 2 --> extended bitcount 
+            
+            if (mode) {
+                n *= 2;
+                if (n < 0){ 
+                    n--;
+                }
+            }
 
+            u_int32_t rem = ((uint32_t)n) % m;              // remainder used in suffix
+            
+            if (pow(2, bitcount) == m) {                // m is a power of 2 --> no extended bitcount
+                for (uint32_t i = bitcount-1; i >= 0; i--) 
+                    vec.push_back(rem >> i);     
+            } else {                                        // m not power of 2 --> extended bitcount 
+                uint32_t ext_symb = m - pow(2, bitcount);   // number of extended symbols -> total symbols == 2 * ext_symb
+                                                            // symbol is extended if rem 
             }
 
             // INSERT SIGN BIT
