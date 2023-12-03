4e8323a8e988f95be4dcdf38e0f8247deb9fff01
negrigabriel <negrigabriel@ua.pt>
2023-12-03 20:17:28 +0000

    entrega final

--- Projeto2/testbitstream.cpp
+++ Projeto2/testbitstream.cpp
@@ -1,12 +1,36 @@
 #include "bitstream.h"
 
+void read(string path) {
+    BitStream rs {path};
+    while(rs.hasNext())
+        cout << rs.readBit();
+}
+
+void write(string path, string bits) {
+    BitStream ws {path};
+    for(auto c: bits) {
+        bool b = (bool) stoi(string {c});
+        cout << b;
+        ws.writeBit(b);
+    }
+}
+
 int main(int argc, char* argv[]) {
-    if (argc != 2){
+    if (argc < 3 || argc > 4) {
         cerr << "wrong arg number\n";
         return 1;
     }
-    BitStream bs {argv[1]};
-    while(bs.hasNext())
-        cout << bs.readBit();
+    
+    switch(argv[1][1]) {
+        case 'r':
+            read(argv[2]);
+            break;
+        case 'w':
+            write(argv[2], argv[3]);
+            break;
+        default:
+            cerr << "invalid operation\n";
+    }
+    
     return 0;
 }
